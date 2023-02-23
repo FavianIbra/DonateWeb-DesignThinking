a32998cd837859d89541c7068285516d44a25a3a
FavianIbra <ibra.yanuarta@gmail.com>
2023-02-23 17:39:08 +0700

    first commit

---
+++ components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import { RxSketchLogo } from 'react-icons/rx';
+
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className='flex'>
+      <div className='fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between'>
+        <div className='flex flex-col items-center'>
+          <Link href='/'>
+            <div className='bg-gray-800 text-white p-3 rounded-lg inline-block'>
+              <RxSketchLogo size={20} />
+            </div>
+          </Link>
+          
+        </div>
+      </div>
+      <main className='ml-20 w-full'>{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ pages/customers.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const customers = () => {
+  return (
+    <div className='bg-gray-100 min-h-screen'>
+      <div className='flex justify-between p-4'>
+        <h2>Customers</h2>
+        <h2>Welcome Back, Clint</h2>
+      </div>
+  
+    </div>
+  );
+};
+
+export default customers;
